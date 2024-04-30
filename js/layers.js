133d2532818b7af43fd648c2385a504a6a7b700c
GamerFlo <f.wallisch@elgym.at>
2024-04-30 18:47:40 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -95,7 +95,7 @@ addLayer("b", {
     baseResource: "oxygen", // Name of resource prestige is based on
     baseAmount() {return player.o.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.4, // Prestige currency exponent
+    exponent: 0.6, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         if (hasUpgrade('b', 13)) mult = mult.times(upgradeEffect('b', 13))
