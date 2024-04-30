bf94dbb97a883659f941e658be80dbf12ae36a30
GamerFlo <f.wallisch@elgym.at>
2024-04-30 19:40:19 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -64,7 +64,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280"))
+	return player.points.gte(new Decimal("e70"))
 }
 
 
