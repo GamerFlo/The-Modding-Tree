59fa842c77ba5e71013a9d6f210a87d628362179
GamerFlo <f.wallisch@elgym.at>
2024-09-01 17:36:36 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -94,7 +94,7 @@ addLayer("b", {
     baseResource: "water points", // Name of resource prestige is based on
     baseAmount() {return player.w.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1.1, // Prestige currency exponent
+    exponent: 0.9, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
 
