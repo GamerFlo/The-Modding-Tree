178aec608df6d141dba4bba6ab0b1c2955c77de7
GamerFlo <f.wallisch@elgym.at>
2024-12-27 11:33:33 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -95,7 +95,6 @@ addLayer("d", {
     exponent: 0, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-        mult = mult.add(ln(player.points))
         if (hasUpgrade('d', 12)) mult = mult.times(upgradeEffect('d', 12))
         return mult
     },
