2b78390e16df6e367ddf30520f155abde9f42a89
GamerFlo <f.wallisch@elgym.at>
2025-01-02 10:20:40 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -43,8 +43,6 @@ function getPointGen() {
 	let gain = new Decimal(0)
 	gain = gain.add(buyableEffect('m', 11))
 	if (hasUpgrade('d', 11)) gain = gain.times(upgradeEffect('d', 11))
-	if (hasUpgrade('m', 11)) gain = gain.times(upgradeEffect('m', 11))
-	if (hasUpgrade('m', 12)) gain = gain.times(upgradeEffect('m', 12))
 	gain = gain.times(buyableEffect('d', 11))
 	return gain
 }
